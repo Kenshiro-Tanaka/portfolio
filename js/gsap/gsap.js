4b0cf8d05eaff1c25671c5fe97e1abc886d7bc71
Kenshiro Tanaka <ke.tanaka@icn.ist.hokudai.ac.jp>
2024-06-07 16:12:39 +0900

    add loading and sushi

--- js/gsap/gsap.js
+++ js/gsap/gsap.js
@@ -1,18 +1,17 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.from(".gsap-target-skill", {
-    y: 100,
-    autoAlpha: 0,
-    delay: 0.6,
-    stagger: {
-        each: 0.2
-      },
-    scrollTrigger: {
-        trigger: ".gsap-scrolltrigger-skill"
-    }
-});
-
 const experiences = gsap.utils.toArray(".gsap-target-experience");
 experiences.forEach((target) => {
+    gsap.from(target, 0.8,{
+        y: 100,
+        autoAlpha: 0,
+        delay: 0.6,
+        scrollTrigger: {
+            trigger: target,
+        }
+    });
+});
+
+const skills = gsap.utils.toArray(".gsap-target-skill");
+skills.forEach((target) => {
     gsap.from(target, 0.8,{
         y: 100,
         autoAlpha: 0,
